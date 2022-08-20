acf742915d0239f60441450c1fc28d21020dfc55
Achraf LOUIZA <achraf.louiza97@gmail.com>
2022-08-20 17:11:05 +0200

    Files refactoring

---
+++ tradingAPI/okex/__init__.py
@@ -0,0 +1,20 @@
+from .client import Client
+from .account_api import AccountAPI
+from .exceptions import OkexAPIException, OkexRequestException, OkexParamsException
+from .funding_api import FundingAPI
+from .market_api import MarketAPI
+from .public_api import PublicAPI
+from .status_api import StatusAPI
+from .sub_account_api import SubAccountAPI
+from .trade_api import TradeAPI 
+from .consts import *
+from .utils import *
+from .config import *
+
+def test_account_api():
+    account = AccountAPI(API_KEY, API_SECRET, PASSPHRASE, flag = 1)
+    print(account)
+        
+
+
+
---
+++ tradingAPI/test.py
@@ -0,0 +1,2 @@
+from okex import Client
+
